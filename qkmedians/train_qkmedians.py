b7bb0a31e596dc960f4d39fb1130c1e24d173d17
emapuljak <ema.puljak@cern.ch>
2022-06-06 23:24:43 +0200

    Update train_qkmedians.py

--- qkmedians/train_qkmedians.py
+++ qkmedians/train_qkmedians.py
@@ -11,54 +11,51 @@ import utils as u
 
 qibo.set_device("/GPU:0")
 
-latent_dim='4' # args1
-train_size=600 #args2
 
-save_dir='/eos/user/e/epuljak/private/epuljak/PhD/TN/QIBO/search_algorithms/notebooks/results_qmedians/corrected_cuts/diJet' #args3
+def train_qkmedians(latent_dim, train_size, read_file, k=2, tolerance=1.e-3, save_file=None):
 
-# read QCD predicted data (test - SIDE)
-read_dir =f'/eos/user/e/epuljak/private/epuljak/public/diJet/{latent_dims[j]}/' #args4
-file_name = 'latentrep_QCD_sig_around35.h5' #args5
-with h5py.File(read_dir+file_name, 'r') as file:
-    data = file['latent_space']
-    l1 = data[:,0,:]
-    l2 = data[:,1,:]
+    # read train data
+    with h5py.File(read_dir+file_name, 'r') as file:
+        data = file['latent_space']
+        l1 = data[:,0,:]
+        l2 = data[:,1,:]
 
-    data_train = np.vstack([l1[:train_size], l2[:train_size]])
-    np.random.shuffle(data_train)
+        data_train = np.vstack([l1[:train_size], l2[:train_size]])
+        np.random.shuffle(data_train)
 
-# TRAIN Q-MEDIANS
-k = 2 # number of clusters #args6
-centroids = qkmed.initialize_centroids(data_train, k)   # Intialize centroids
-tolerance=1.e-3 # args7
+    # train qkmedians
+    centroids = qkmed.initialize_centroids(data_train, k)   # Intialize centroids
 
-# run k-medians algorithm
-i = 0
-loss=[]
-while True:
-    cluster_label, _ = qkmed.find_nearest_neighbour_DI(data_train,centroids)       # find nearest centers
-    print(f'Found cluster assignments for iteration: {i+1}')
-    new_centroids = qkmed.find_centroids_GM(data_train, cluster_label, clusters=k)               # find centroids
+    i = 0
+    loss=[]
+    while True:
+        cluster_label, _ = qkmed.find_nearest_neighbour_DI(data_train,centroids)       # find nearest centers
+        print(f'Found cluster assignments for iteration: {i+1}')
+        new_centroids = qkmed.find_centroids_GM(data_train, cluster_label, clusters=k)               # find centroids
 
-    loss_epoch = np.linalg.norm(centroids - new_centroids)
-    loss.append(loss_epoch)
+        loss_epoch = np.linalg.norm(centroids - new_centroids)
+        loss.append(loss_epoch)
 
-    if loss_epoch < tolerance:
+        if loss_epoch < tolerance:
+            centroids = new_centroids
+            print(f"Converged after {i+1} iterations.")
+            break
+        i += 1     
         centroids = new_centroids
-        print(f"Converged after {i+1} iterations.")
-        break
-        
-    print(f'Iteration: {i+1}')
-    if i == 200: 
-        print('QKmedians stopped after 200 epochs!')
-        centroids = new_centroids
-        break
-    i += 1     
-    centroids = new_centroids
 
-np.save(f'{save_dir}/cluster_labels/around35/cluster_label_lat{latent_dims[j]}_{str(n_train_samples)}.npy', cluster_label)
-np.save(f'{save_dir}/centroids/around35/centroids_lat{latent_dims[j]}_{str(n_train_samples)}.npy', centroids)
-np.save(f'{save_dir}/loss/around35/LOSS_lat{latent_dims[j]}_{str(n_train_samples)}.npy', loss)
-print('Centroids and labels saved!')
+    np.save(f'{save_dir}/cluster_label_lat{latent_dim}_{str(train_size)}.npy', cluster_label)
+    np.save(f'{save_dir}/centroids_lat{latent_dim]}_{str(train_size)}.npy', centroids)
+    np.save(f'{save_dir}/LOSS_lat{latent_dim}_{str(train_size)}.npy', loss)
+    print('Centroids and labels saved!')
 
-# read args .... TODO
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='read arguments for qkmedians training')
+    parser.add_argument('-lat_dim', dest='latent_dim', type=str, help='latent dimension')
+    parser.add_argument('-train_size', dest='train_size', type=int, help='training data size')
+    parser.add_argument('-read_file', dest='read_file', type=str)
+    parser.add_argument('-k', dest='k', type=int, default=2, help='number of classes')
+    parser.add_argument('-tolerance', dest='tolerance', type=float, help='tolerance')
+    parser.add_argument('-save_file', dest='save_file',type=str, help='save file')
+
+    args = parser.parse_args()
