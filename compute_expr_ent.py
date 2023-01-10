bf9f7c0822bf9065ad1bff191e8d20fc7b1029ad
Vasilis Belis <billbelis@yahoo.com>
2023-01-10 17:26:00 +0100

    WIP, add switcher and data-dependent expr vs. qubits computation

--- compute_expr_ent.py
+++ compute_expr_ent.py
@@ -37,61 +37,27 @@ def main(args):
         lambda x: u_dense_encoding_all(x, nqubits=args["n_qubits"], reps=3)
     )
     circuit_labels = ['NE_0', 'NE_1', 'L=1', 'L=2', 'L=3', 'L=4', 'L=5', 'L=6', 'FE']
-    
-    sig_data, bkg_data = get_data(args["sig_path"], args["bkg_path"])
-    
-    compute_expr_ent_vs_circuit(args, circuit_list_expr_ent, circuit_labels, 
-                                data=sig_data)
-    
-    '''
-    # Expr. and Ent. as a function of the circuit depth and amount of CNOT gates
-    print("\nComputing expressibility and entanglement capability of the circuits, "
-          f"for {args['n_exp']} evaluations and n_shots = {args['n_shots']}." 
-          f"\nCircuits: {circuit_labels}")
-    train_time_init = perf_counter()
-    for i, circuit in enumerate(circuit_list_expr_ent):
-        print(f"\nFor circuit {circuit_labels[i]}...")
-        expr = []
-        
-        # Compute entanglement cap. only once, it fluctuates less than 0.1%
-        # and does not require that high statistics
-        if circuit_labels[i] in ['NE_0', 'NE_1']:
-            val_ent = 0 # by construction
-        else:
-            val_ent = entanglement_capability(circuit, n_params, n_shots=10)
-        
-        for _ in range(args["n_exp"]):
-            val_ex = expressibility(circuit, n_params, method='full', 
-                                    n_shots=args['n_shots'], n_bins=75, data=bkg_data)
-            expr.append(val_ex)
-        expr = np.array(expr)
-        ent = np.array(val_ent)
-        print(f"expr = {np.mean(expr)} ± {np.std(expr)}")
-        print(f"ent = {np.mean(ent)} ± {np.std(ent)}")
-        
-        d = {
-            "circuit": circuit_labels[i],
-            "expr": np.mean(expr),
-            "expr_err": np.std(expr),
-            "ent": np.mean(ent),
-            "ent_err": np.std(ent),
-        }
-        df_expr_ent = df_expr_ent.append(d, ignore_index=True)
-        
-    train_time_fina = perf_counter()
-    exec_time = train_time_fina - train_time_init
-    print(
-        "\nFull computation completed in: " + tcols.OKGREEN + f"{exec_time:.2e} sec. "
-        f"or {exec_time/60:.2e} min. " + tcols.ENDC + tcols.SPARKS
-    )
-    print("\nResults: \n", df_expr_ent)
-    df_expr_ent.to_csv(f"{args['out_path']}.csv")
-    '''
-    # TODO Expr. vs. n_qubits. (For exponential concentration)
+    data = None 
+
+    switcher = {
+        "expr_ent_vs_circ": lambda: compute_expr_ent_vs_circuit(
+            args, 
+            circuit_list_expr_ent,
+            circuit_labels, 
+        ),
+        "expr_vs_qubits": lambda: expr_vs_qubits(
+            args=args,
+        )
+    }
+    switcher.get(args["compute"], lambda: None)()
+    exit(1)
+ #   if args["compute"] == "expr_vs_qubits":
+    expr_vs_nqubits(args=args, data=data)
     # TODO variance of the kernel matrix vs. n_qubits (For exponential concentration)
 
+
 def compute_expr_ent_vs_circuit(args: dict, circuits: List[callable], 
-                                circuit_labels: List[str], data=None):
+                                circuit_labels: List[str]):
     """
     Computes the expressibility and entanglement capability of a list of circuits, in 
     the conventional (uniformly sampled parameters from [0, 2pi]) and data-dependent manner.
@@ -99,18 +65,18 @@ def compute_expr_ent_vs_circuit(args: dict, circuits: List[callable],
     print("\nComputing expressibility and entanglement capability of the circuits, "
           f"for {args['n_exp']} evaluations and n_shots = {args['n_shots']}." 
           f"\nCircuits: {circuit_labels}")
-    
+    data = None
+    if args["data_dependent"]: 
+        data = get_data(args["data_path"])
     n_params = 2*args["n_qubits"]
     df_expr_ent = pd.DataFrame(columns=['circuit', 'expr', 'expr_err', 
                                'ent', 'ent_err'])
     
     # Expr. and Ent. as a function of the circuit depth and amount of CNOT gates
-    
     train_time_init = perf_counter()
     for i, circuit in enumerate(circuits):
         print(f"\nFor circuit {circuit_labels[i]}...")
         expr = []
-        
         # Compute entanglement cap. only once, it fluctuates less than 0.1%
         # and does not require that high statistics
         if circuit_labels[i] in ['NE_0', 'NE_1']:
@@ -144,24 +110,75 @@ def compute_expr_ent_vs_circuit(args: dict, circuits: List[callable],
     )
     print("\nResults: \n", df_expr_ent)
     df_expr_ent.to_csv(f"{args['out_path']}.csv")
+    print(f"Saving in {args['out_path']}.csv " + tcols.ROCKET)
 
 
-def get_data(sig_path: str, bkg_path: str) -> Tuple[np.ndarray]:
+def expr_vs_nqubits(args: dict rep: int = 3, 
+                    n_qubits: List = range(1, 13),
+                    n_exp: str = 20):
     """
-    Loads the data, signal and background, given a path and returns the scaled to 
+    Computes the (data-dependent) expressibility of a data embedding circuit as a 
+    function of the qubit number. Saves the output in a dataframe (.h5) with the 
+    computed mean values and uncertainties.
+    
+    Args:
+        data: List of the datasets available for n_qubits = 4, 8, 16 or None for 
+              computation with uniformly sampled circuit parameters [0, 2pi].
+    """
+    print(f"Computing expressibility as a function of the qubit number, "
+          f"for rep = {rep} of the data encoding circuit ansatz.")
+    print(f"Calculating for n_qubits = {n_qubits}...")
+    df_expr = pd.DataFrame(columns=['expr', 'expr_err'])
+    
+    data = None
+    if args["data_dependent"]: 
+        data = get_data(args["data_path"]) # FIXME get a list of lat4, lat8, lat16
+
+    train_time_init = perf_counter()
+    for n in n_qubits:
+        print(f"For n_qubits = {n}: ", end='')
+        n_params = 2*n
+        expr = []
+        for _ in range(args["n_exp"]):
+            val = expressibility(lambda x : u_dense_encoding(x, nqubits=n, reps=rep), 
+                                 method="full", n_params=n_params, n_shots=args["n_shots"],
+                                 n_bins=75, data=data)
+            expr.append(val)
+        
+        expr = np.array(expr)
+        print(f"expr = {np.mean(expr)} ± {np.std(expr)}")
+        d = {
+            "expr": np.mean(expr),
+            "expr_err": np.std(expr),
+        }
+        df_expr = df_expr.append(d, ignore_index=True)
+    
+    train_time_fina = perf_counter()
+    exec_time = train_time_fina - train_time_init    
+    print(
+        "\nFull computation completed in: " + tcols.OKGREEN + f"{exec_time:.2e} sec. "
+        f"or {exec_time/60:.2e} min. " + tcols.ENDC + tcols.SPARKS
+    )
+    print("\nResults: \n", df_expr)
+    df_expr.to_csv(f"{args['out_path']}.csv")
+    print(f"Saving in {args['out_path']}.csv " + tcols.ROCKET)
+
+
+def get_data(data_path: str) -> Tuple[np.ndarray]:
+    """
+    Loads the data, signal or background, given a path and returns the scaled to 
     (0, 2pi) numpy arrays.
     """
-    print(tcols.BOLD + "\nLoading signal and background datasets... " + tcols.ENDC)
-    h5_sig, h5_bkg = h5py.File(sig_path, "r"), h5py.File(bkg_path, "r")
-    sig, bkg = np.asarray(h5_sig.get("latent_space")), np.asarray(h5_bkg.get("latent_space"))
-    sig, bkg = np.reshape(sig, (len(sig), -1)), np.reshape(bkg, (len(bkg), -1))
-    print(f"Signal array shape {sig.shape} and background array shape {bkg.shape}.")
+    print(tcols.BOLD + f"\nLoading the dataset {data_path}... " + tcols.ENDC)
+    h5_data = h5py.File(data_path, "r")
+    data= np.asarray(h5_data.get("latent_space"))
+    data= np.reshape(data, (len(data), -1))
+    print(f"Loaded data array of shape {data.shape}")
+    
     # rescaling to (0, 2pi) needed for the expr. and ent. computations.
-    sig *= np.pi
-    sig += np.pi
-    bkg *= np.pi
-    bkg += np.pi
-    return sig, bkg
+    data *= np.pi
+    data += np.pi
+    return data
 
 
 def u_dense_encoding_no_ent(x, nqubits: int = 8, reps: int = 3, type: int = 0
@@ -286,29 +303,7 @@ n_shots = int(10000)
 #        print(f"{i}: expr = {val}")
 #    ent = np.array(ent)
 #    print(f"ent = {np.mean(ent)} ± {np.std(ent)}")   
-
-#rep=3
-#print(f"Computing expressibility as a function of the qubit number, for rep = {rep} of the ansatz...")
-#expr = []
-#hist = 0
-#for n in range(16,17):
-#    print(f"For n = {n}: ", end='')
-#    n_params = 2*n
-#    val, hist = expressibility(lambda x : u_dense_encoding(x, nqubits=n, reps=rep), 
-#                         n_params, n_shots=n_shots, n_bins=75, return_histogram=True)
-#    expr.append(val)
-#    print(f"expr = {val}")
-#expr = np.array(expr)
-#np.save("expr_qubits.npy", expr)
-#print(expr)
-#print(f"histogram values: {hist}")
 '''
-
-def expr_vs_nqubits():
-    """Computes the (data-dependent) expressibility of a data embedding circuit as a 
-    function of the qubit number
-    """
-    pass 
 
 
 def get_arguments() -> dict:
@@ -345,14 +340,24 @@ def get_arguments() -> dict:
         help="Output dataframe to be used for plotting.",
     )
     parser.add_argument(
-        "--sig_path",
+        "--data_path",
         type=str,
-        help="Path to signal (anomaly) dataset (.h5 file) to be used in expr. calculation",
+        help="Path to signal dataset (background or signal .h5 file) to be used in "
+             "expr. calculation",
     )
     parser.add_argument(
-        "--bkg_path",
+        "--compute",
         type=str,
-        help="Path to background (anomaly) dataset (.h5 file) to be used in expr. calculation",
+        required=True,
+        choices=["expr_ent_vs_circ", "expr_vs_qubits", "var_kernel_vs_circ"],
+        help="Run different calculations: compute expressibility and entanglement "
+        "capability of different circuits, compute expressibility as a function of the "
+        "number of qubits, and TODO",
+    )
+    parser.add_argument(
+        "--data_dependent",
+        action="store_true",
+        help="Compute the expressibility as a data-dependent quantity",
     )
     args = parser.parse_args()
     args = {
@@ -360,8 +365,9 @@ def get_arguments() -> dict:
         "n_shots": args.n_shots,
         "n_exp": args.n_exp,
         "out_path": args.out_path,
-        "sig_path": args.sig_path,
-        "bkg_path": args.bkg_path,
+        "data_path": args.data_path,
+        "compute": args.compute,
+        "data_dependent": args.data_dependent,
     }
 
     return args
