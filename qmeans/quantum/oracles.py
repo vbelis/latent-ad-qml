bf82c31015c0bce270f7908942181e19e55f58fe
kingusiu <kinga.wozniak@gmail.com>
2023-02-07 23:11:53 +0100

    adding comment to oracle module

--- qmeans/quantum/oracles.py
+++ qmeans/quantum/oracles.py
@@ -8,18 +8,44 @@ from qiskit.quantum_info.operators import Operator, Pauli
 
 
 def create_threshold_oracle_operator(nn, idcs_to_mark):
+    """Threshold oracle operator for Grover circuit
 
-    # create the identity matrix on n qubits
+    Parameters
+    ----------
+    nn : int
+        number of qubits
+    idcs_to_mark : int
+        number of elements to mark
+
+    Returns
+    -------
+    Operator
+        Threshold oracle.
+    """
+
     oracle_matrix = np.identity(2**nn)
-    # add the -1 phase to marked elements
+
     for idx in idcs_to_mark:
         oracle_matrix[idx, idx] = -1
 
-    # convert oracle_matrix into an operator, and add it to the quantum circuit
     return Operator(oracle_matrix)
 
 
 def get_indices_to_mark(dist_arr, threshold):
+    """utility function that finds indices to be marked
+
+    Parameters
+    ----------
+    dist_arr : np.ndarray
+        array of distances.
+    threshold: float
+        current threshold
+
+    Returns
+    -------
+    np.ndarray
+        the marked indices
+    """
     (idx,) = np.nonzero(dist_arr < threshold)
     if idx.size == 0:  # handle minimum element
         idx = [np.argmin(dist_arr)]
@@ -27,10 +53,17 @@ def get_indices_to_mark(dist_arr, threshold):
 
 
 def create_threshold_oracle_set(dist_arr):
+    """Create set of threshold oracles for linear combination
 
-    """
-    create set of threshold oracles {O_1, ..., O_m} for m possible thresholds
-    where oracle_t marks all indices i for which f(i) < threshold t
+    Parameters
+    ----------
+    dist_arr : np.ndarray
+        array of distances.
+
+    Returns
+    -------
+    List
+        List of threshold oracle Operators
     """
 
     cluster_n = len(dist_arr)
@@ -47,13 +80,25 @@ def create_threshold_oracle_set(dist_arr):
 
 
 def create_oracle_lincombi(threshold, dist_arr, oracles):
+    """Create linear combination of threshold oracles for Grover circuit
 
-    """join oracles from oracle set with delta function for a given threshold (pick oracles to use)"""
+    Parameters
+    ----------
+    threshold : float
+        current threshold value
+    dist_arr : np.ndarray
+        array of distances.
+    oracles: List
+        list of oracle Operators
 
-    # create delta-coefficients for linear combi sum_a delta(y - a) * O_a
-    idx = np.where(dist_arr == threshold)[0][
-        0
-    ]  # taking first match if equal distances present
+    Returns
+    -------
+    QuantumCircuit
+        The linear combination threshold oracle circuit.
+    """
+
+
+    idx = np.where(dist_arr == threshold)[0][0]
     delta_coeff = signal.unit_impulse(len(dist_arr), idx)
 
     oracle_sum = delta_coeff[0] * oracles[0]
@@ -62,11 +107,8 @@ def create_oracle_lincombi(threshold, dist_arr, oracles):
 
     nn = int(math.floor(math.log2(len(dist_arr)) + 1))
 
-    # create a quantum circuit on nn qubits
     qc = QuantumCircuit(nn, name="combi_oracle")
 
-    # import ipdb; ipdb.set_trace()
-    # convert oracle_matrix into an operator, and add it to the quantum circuit
     qc.unitary(oracle_sum, range(nn))
 
     return qc
