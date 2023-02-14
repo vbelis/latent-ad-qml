801c31300da79f334f11e865d3a1fe31c05af9e7
epuljak <ema.puljak@cern.ch>
2023-02-14 16:14:18 +0100

    Edit `setup.py`

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(
     name='qad',
-    description='QML',
-    author='Ema',
+    description='Quantum anomaly detection in the latent space of proton collision events',
+    author='Vasilis Belis, Ema Puljak, Kinga Anna Wozniak',
 )
