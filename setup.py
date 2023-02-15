83208058394200c1fee162ffcd0eb6c82aa5872e
epuljak <ema.puljak@cern.ch>
2023-02-15 13:30:48 +0100

    Add metadata in `setup.py`

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='qad',
+    version='1.0',
     description='Quantum anomaly detection in the latent space of proton collision events',
     author='Vasilis Belis, Ema Puljak, Kinga Anna Wozniak',
+    packages=find_packages(exclude=['scripts']),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
 )
