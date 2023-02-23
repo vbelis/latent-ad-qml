83bf51d9b12745563b54fbab91481681d11560f2
epuljak <ema.puljak@cern.ch>
2023-02-23 18:10:16 +0100

    Edit `setup.py`

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.8",
+    python_requires=">=3.7",
     install_requires=[
         "h5py>=3.6.0",
         "joblib>=1.2.0",
